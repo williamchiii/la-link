321ea2d2f392c6c1d81725a9e413fdce90a011f5
William Chi <chi.will@outlook.com>
2026-01-08 04:45:01 -0500

    feat: add HomeAuthPage component with styling and dependencies for Bootstrap and React OAuth

---
+++ frontend/src/pages/HomeAuthPage.jsx
@@ -0,0 +1,15 @@
+import "./HomeAuthPage.css";
+
+function HomeAuthpage(){
+    return (
+    <div className="background">
+        <h1 className="mainTitle">LaLink</h1>
+        <p className="bodyText">
+            A simple and quick to use multifunctional URL shortener
+        </p>
+    </div>
+    
+);
+}
+
+export default HomeAuthpage;
