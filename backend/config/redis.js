c9a329c09ff5dad234b24bf5e17bd2082668969f
William Chi <williamc28275311@yahoo.com>
2026-03-05 17:51:18 -0500

    test commit to test CI functions

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -3,6 +3,8 @@ import {Redis} from "@upstash/redis";
 import dotenv from "dotenv";
 dotenv.config();
 
+//This is used for ratelimiting and caching
+
 //retrieve and export redis creds
 export const redis = Redis.fromEnv()
 //rate limiter that allows 60 requests per minute.
